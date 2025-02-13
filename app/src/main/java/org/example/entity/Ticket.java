31cd5beb144d250d5cc4cffde4d672ccc18f7d97
omkar-ugal <omkarugalmugle285@gmail.com>
2025-02-13 11:39:23 +0530

    Initial commit

---
+++ app/src/main/java/org/example/entity/Ticket.java
@@ -0,0 +1,15 @@
+package org.example.entity;
+
+public class Ticket {
+    private String ticketId;
+
+    private String userId;
+
+    private String source;
+
+    private String destination;
+
+    private Date dateOfTravel;
+
+    private Train train;
+}
---
+++ app/src/main/java/org/example/entity/User.java
@@ -0,0 +1,16 @@
+package org.example.entity;
+
+import java.util.*;
+
+public class User {
+    private String name;
+
+    private String password;
+
+    private String hashPassword;
+
+    private List<Ticket> ticketBooked;
+
+    private String userId;
+
+}
